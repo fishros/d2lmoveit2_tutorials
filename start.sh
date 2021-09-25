6f4c718b4d1cfabe459596b45a39b8bd12b47ebc
sangxin <sangxin2014@sina.com>
2021-09-25 23:43:03 +0800

    修改依赖

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@ sudo apt-get update
 sudo apt-get install curl
 curl http://fishros.com/tools/install/rosdepc | bash 
 sudo apt-get install ros-foxy-moveit
-rosdepc install -r --from-paths . --ignore-src --rosdistro $ROS_DISTRO -y
+rosdepc install -r --from-paths . --ignore-src --skip-keys=rviz_visual_tools --rosdistro $ROS_DISTRO -y 
 sudo apt install ros-foxy-joint-state-controller
 colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release
